7b46d7dde473fec359083224208990444e53d95f
BigBadMosquito <komarov2500@mail.ru>
2026-04-15 13:56:38 +0300

    add constructors

--- src/double_linked_list.cpp
+++ src/double_linked_list.cpp
@@ -7,6 +7,15 @@ DoubleLinkedList::~DoubleLinkedList(){
     Clear();
 }
 
+DoubleLinkedList::DoubleLinkedList(DoubleLinkedList&& other):
+    head_(std::move(other.head_)){}
+
+DoubleLinkedList& DoubleLinkedList::operator=(DoubleLinkedList&& other) noexcept{
+    DoubleLinkedList tpm(std::move(other));
+    this->swap(tpm);
+    return *this;
+}
+
 void DoubleLinkedList::Clear() noexcept{
     while(head_->next){
         auto node = head_->next;
@@ -16,3 +25,12 @@ void DoubleLinkedList::Clear() noexcept{
 
     delete head_;
 }
+
+// void DoubleLinkedList::PushBack(std::string data, int rand_index){
+
+// }
+
+void DoubleLinkedList::swap(DoubleLinkedList& other) noexcept {
+    std::swap( head_->next, other.head_->next);
+}
+
--- src/double_linked_list.h
+++ src/double_linked_list.h
@@ -5,11 +5,19 @@
 class DoubleLinkedList {
 public:
     DoubleLinkedList();
-
     ~DoubleLinkedList();
+
+    DoubleLinkedList(const DoubleLinkedList&) = delete;
+    DoubleLinkedList& operator=(const DoubleLinkedList&) = delete;
+    
+    DoubleLinkedList(DoubleLinkedList&& other) noexcept;
+    DoubleLinkedList& operator=(DoubleLinkedList&& other) noexcept;
 
     void Clear() noexcept;
 
+    // void PushBack(std::string data, int rand_index);
+
+    void swap(DoubleLinkedList & other) noexcept ;
 private:
     ListNode* head_ = nullptr;
 
